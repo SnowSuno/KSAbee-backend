28f56ec502a6600d5ebff84562a0b0ca76f1592b
SnowSuno <percy3368@gmail.com>
2021-06-28 03:53:20 +0900

    feat : add renew method

--- app/modules/op-gg.py
+++ app/modules/op-gg.py
@@ -2,11 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_user_info(user_name):
+def renew(summoner_id: str):
+    url = 'https://www.op.gg/summoner/ajax/renew.json/'
+
+    response = requests.post(url, data={'summonerId': summoner_id})
+
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+
+
+def get_user_info(user_name: str):
     url = f'https://www.op.gg/summoner/userName={user_name}'
 
     user_data = {
         'nickname': '',
+        'summonerId': 0,
         'profileImage': '',
         'level': 0,
         'tier': '',
@@ -28,6 +41,7 @@ def get_user_info(user_name):
             return {}
 
         user_data['nickname'] = soup.select_one('.Profile>.Information>.Name').text
+        user_data['summonerId'] = soup.select_one('.GameListContainer')['data-summoner-id']
         user_data['profileImage'] = "https:" + soup.select_one('.ProfileImage')['src']
         user_data['level'] = int(soup.select_one('.Level').text)
 
@@ -50,6 +64,7 @@ def get_user_info(user_name):
 # test code
 if __name__ == '__main__':
     from pprint import pprint
-    pprint(get_user_info('침대에서뒹굴'))     # Unranked user
-    pprint(get_user_info('마리마리착마리'))   # Ranked user
-    pprint(get_user_info('qwerqwefdsad'))  # Unexisting user
+    # pprint(get_user_info('침대에서뒹굴'))     # Unranked user
+    # pprint(get_user_info('마리마리착마리'))   # Ranked user
+    # pprint(get_user_info('qwerqwefdsad'))  # Unexisting user
+
